2fb384c22fe678de71c1a7e76a3868372063f5d2
brennency <98924000+brennency@users.noreply.github.com>
2023-01-09 22:47:12 -0500

    Delete build directory

--- build/javascript/login-form.js
+++
@@ -1,9 +0,0 @@
-
-jQuery(document).ready(() => {
-    const key = crypto.randomUUID();
-    jQuery('#auth-key').val(key);
-
-    jQuery('#login-button').on('click', () => {
-        jQuery('#login-form').submit();
-    });
-})
